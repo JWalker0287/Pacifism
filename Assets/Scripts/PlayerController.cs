da895452ce9a677e012d886f2c5e2f2704dae776
Jonathan Walker <jwsmasher@gmail.com>
2021-03-16 13:51:44 -0500

    Changed player movement to use AddForce instead of directly changing velocity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
-
     public float turnSpeed = 20;
+    public float maxVelocityChange = 1;
     Rigidbody body;
     void Awake()
     {
         body = GetComponent<Rigidbody>();
     }
+    Vector3 dir;
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
-        Vector3 dir = new Vector3(x,0,z).normalized;
-        body.velocity = new Vector3(x,0,z).normalized * speed;
-
+        dir = new Vector3(x,0,z).normalized;
+        
         if (body.velocity.sqrMagnitude > 0.1f)
         {
-            transform.forward = Vector3.Lerp(transform.forward, dir, Time.deltaTime * turnSpeed);
+            transform.forward = Vector3.Lerp(
+                transform.forward, 
+                dir, 
+                Time.deltaTime * turnSpeed
+            );
         }
+    }
+    void FixedUpdate ()
+    {
+        Vector3 velocityChange = dir * speed - body.velocity;
+        velocityChange.y = 0;
+        if (velocityChange.magnitude > maxVelocityChange)
+        {
+            velocityChange = velocityChange.normalized * maxVelocityChange;
+        }
+        body.AddForce(velocityChange, ForceMode.VelocityChange);
     }
 }
