67a0dbaee67255b4bc6adf5fa08421c6122e76d1
Jonathan Walker <jwsmasher@gmail.com>
2021-03-18 14:41:49 -0500

    Made Enemyspawner that spawns enemies at different spawnpoints

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EnemyController prefab;
+    EnemyController[] pool;
+    public int poolSize = 100;
+    public int spawnCount = 3;
+    public float spawnInterval = 3;
+    public Transform[] spawnPoints;
+    IEnumerator Start()
     {
-        
+        FillPool();
+        while (enabled)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            SpawnEnemies();
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    void FillPool ()
     {
-        
+        pool = new EnemyController[poolSize];
+        for (int i = 0; i < poolSize; i ++)
+        {
+            EnemyController enemy = Instantiate<EnemyController>(prefab);
+            pool[i] = enemy;
+            enemy.gameObject.SetActive(false);
+            enemy.transform.SetParent(transform);
+        }
+    }
+    EnemyController SpawnEnemy ()
+    {
+        for (int i = 0;i < poolSize;i ++)
+        {
+            EnemyController enemy = pool[i];
+            if (!enemy.gameObject.activeSelf)
+            {
+                return enemy;
+            }
+        }
+        return null;
+    }
+    void SpawnEnemies ()
+    {
+        Transform t = spawnPoints[Random.Range(0,spawnPoints.Length)];
+        for (int i = 0; i < spawnCount; i ++)
+        {
+            EnemyController enemy = SpawnEnemy();
+            Vector3 rand =  Random.insideUnitSphere * 5;
+            rand.y = 0;
+            enemy.transform.position = t.position + rand;
+            enemy.gameObject.SetActive(true);
+        }
     }
 }
