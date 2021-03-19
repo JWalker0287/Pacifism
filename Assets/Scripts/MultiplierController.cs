d95efbaf2421039392b94128edd8035baa9e76ba
Jonathan Walker <jwsmasher@gmail.com>
2021-03-19 15:03:56 -0500

    Started on Multplier

---
+++ Assets/Scripts/MultiplierController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterMotor))]
+public class MultiplierController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
