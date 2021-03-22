c76427f6974b4de85fb6f65a0319e4f44e54758e
Jonathan Walker <jwsmasher@gmail.com>
2021-03-22 14:50:32 -0500

    Made multipliers stop after the player gets too far from them; made multipliers disappear after a few seconds; added animation to multipliers; made play area smaller; changed fonts on ui elements; multiplier is now x1 at the beginning of the game

--- Assets/Scripts/MultiplierController.cs
+++ Assets/Scripts/MultiplierController.cs
@@ -7,9 +7,22 @@ public class MultiplierController : MonoBehaviour
 {
     CharacterMotor motor;
     public float minDist = 10;
+    public float timeBeforeDisable = 10;
+    Animator anim;
     void Awake()
     {
+        anim = GetComponent<Animator>();
         motor = GetComponent<CharacterMotor>();
+    }
+    void OnEnable ()
+    {
+        anim.SetTrigger("TimeOut");
+        StartCoroutine(DeSpawnMultiplier());
+    }
+    IEnumerator DeSpawnMultiplier ()
+    {
+        yield return new WaitForSeconds(timeBeforeDisable);
+        gameObject.SetActive(false);
     }
     void Update()
     {
@@ -18,6 +31,10 @@ public class MultiplierController : MonoBehaviour
         if (distSq < minDist * minDist)
         {
             motor.dir = diff/ Mathf.Sqrt(distSq);
+        }
+        else
+        {
+            motor.dir = Vector3.zero;
         }
     }
     void OnTriggerEnter(Collider c)
