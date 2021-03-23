28a70166c60a547c93b73836b97292a7f3a73afc
Jonathan Walker <jwsmasher@gmail.com>
2021-03-23 10:56:39 -0500

    Animated Explosions; improved menu screen

--- Assets/Scripts/ExplosionController.cs
+++ Assets/Scripts/ExplosionController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class ExplosionController : MonoBehaviour
 {
+    Animator anim;
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
     void OnEnable()
     {
         StartCoroutine(Explode());
     }
     IEnumerator Explode()
     {
+        anim.SetTrigger("Explosion");
         yield return new WaitForSeconds(0.1f);
         gameObject.SetActive(false);
     }
